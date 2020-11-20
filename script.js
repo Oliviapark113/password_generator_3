042c371e53d7479c385ff943229e7e654d9bf083
Olivia Park <oliviaypark113@gmail.com>
2020-11-20 15:11:41 -0500

    revised version

--- script.js
+++ script.js
@@ -1,76 +1,86 @@
 
 
 // Assignment Code
+
 "use strict;";
+
 
 let generateBtn = document.querySelector("#generate");
 let errorMsg = document.querySelector("#error");
 let passwordInput = document.querySelector("#password")
 
-let passwordText =
-    "ABCDEFGHIJKLMNOPQRSTUVWYZabcdefghijklmnopqrstuvwxyz'!@#$%^&*=-_' 0123456789";
 
-let questions = {
-    q1: () => prompt("what is your mothers' maiden name?"),
-    q2: () => prompt("What is the name of your pet?"),
-    q3: () => prompt("What city were you born ?"),
-    q4: () => confirm("Please add uppercase , lowercase and numbers and symbols"),
-    q5: () => prompt("Please choose password character a length at least 8 but less than 128")
-}
+let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "Y", "Z"];
+
+let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "z"];
+
+let specialChar = ["!", "@", "#", "%", "^", "&", "*", "(", ")", "-", "+", "=", "_", "~", "`"];
+
+let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
 
 
-// Fuctions 
-
+//function
 function writePassword() {
-    //reset before ask questions 
-    reset();
-    let answer1 = questions.q1();
-    if (answer1 === "" || answer1 == "null") {
-        alert("Please enter valid password!")
-    }
-    else {
-        let answer2 = questions.q2();
-        if (answer2 === "" || answer2 == "null") {
-            alert("Please enter valid password!")
 
+    //reset before questions ..
+
+    // WHEN prompted for the length of the password
+    // THEN I choose a length of at least 8 characters and no more than 128 characters
+    let charCount = parseInt(prompt("Please choose password character a length at least 8 but less than 128"));
+
+    if (charCount < 7 || charCount > 127) {
+        alert("Password character should be in 8 - 128 characters..Please enter vailid number")
+        errorMsg.innerHTML = "Password character should be in 8 - 128 characters..Please enter vailid number";
+        errorMsg.setAttribute("class", "");
+    }
+
+    else if (charCount >= 8 && charCount <= 128) {
+        reset();
+        let finalChar = []
+        // WHEN prompted for character types to include in the password
+        // THEN I choose lowercase, uppercase, numeric, and/or special characters
+        let includeLowerCase = prompt("what is your mothers' maiden name? Please answer  MUST include 'lowercase letter' ");
+
+        let includeUpperCase = prompt("What is the name of your pet?  Please answer MUST include 'uppercase letter' ");
+
+        let includeSpecial = prompt("please enter special charater");
+
+        let includeNumber = prompt("please enter numbers");
+
+        if (includeLowerCase) {
+            finalChar.push(includeLowerCase);
+            console.log(finalChar);
         }
-        else {
-            let answer3 = questions.q3();
-            if (answer3 === "" || answer3 == "null") {
-                alert("Please enter valid password!")
-            }
-            else {
-                let answer4 = questions.q4();
-                if (answer4) {
-                    var answer5 = prompt("Please choose password character a length at least 8 but less than 128")
 
-                }
-            }
+        if (includeUpperCase) {
+            finalChar.push(includeUpperCase);
         }
+
+        if (includeSpecial) {
+            finalChar.push(includeSpecial);
+        }
+
+
+        if (includeNumber) {
+            finalChar.push(includeNumber);
+        }
+
+        console.log(finalChar);
+        var password = finalChar.join("")
+
+        passwordInput.value = "Your Password has been set Up \n" + password
+
     }
 
-    if (answer5.length <= 7 || answer5.length >= 129) {
-        alert(answer5 + " is not a character between 8 - 128. Try Again!")
-        errorMsg.textContent = "Please enter valid code";
-       
-    }
-    else {
-
-        alert("Your Password is " + answer5 + "!");
-        let passwordInput = document.querySelector("#password");
-        passwordInput.innerHTML = "Your Password is " + answer5 + "!";
-
-    }
 
 }
-
 
 function reset() {
-    errorMsg.textContent = "";
-    passwordInput.textContent = "";
+
+    errorMsg.setAttribute("class", "hide");
+    passwordInput.value = "";
 }
 
 
-// Add eventlistener to generate button//
 generateBtn.addEventListener("click", writePassword);
 
